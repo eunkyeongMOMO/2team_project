61e7234311a3fc4051d84b9b89d75d9fc0bc2321
eunkyeong <eunkyeong91@hanmail.net>
2023-07-23 00:59:56 +0900

    add. 230723_react, ts, scss setting

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,12 @@
+import styles from "../scss/layout.scss";
+import React, {useState, useEffect} from "react";
+
+const Calendar = () =>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Calendar;
---
+++ src/components/TodoInput.tsx
@@ -0,0 +1,12 @@
+import styles from "@src/scss/layout.scss";
+import React, {useState, useEffect} from "react";
+
+const TodoInput = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default TodoInput;
