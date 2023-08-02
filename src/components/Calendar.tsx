4473177b5c728d055d1f26f473f08d87f9c77556
eunkyeong <eunkyeong91@hanmail.net>
2023-08-02 23:27:24 +0900

    add.230802_moment.js설치

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,7 +1,10 @@
 import styles from "../scss/layout.scss";
 import React, {useState, useEffect} from "react";
-
+import moment from "moment";
 const Calendar = () =>{
+    let now = moment();
+    now.format("YY/MM/DD");
+    const week:string[] =['월','화','수','목','금','토','일'];
     return(
         <div>
             
