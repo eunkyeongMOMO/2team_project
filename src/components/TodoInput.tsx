71522fb4dede5fe16e3db25b5e2111010975d48f
eunkyeong <eunkyeong91@hanmail.net>
2023-08-03 22:45:47 +0900

    add. 230803_input component add

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -2,11 +2,18 @@ import styles from "@src/scss/layout.scss";
 import React, {useState, useEffect} from "react";
 
 const TodoInput = () =>{
+    const [todo, setTodo] = useState<string[]>([]);
+    const [inputValue, setInputValue] =useState<string>('');
+    const addTodo = ()=>{
+        let todoCopy = [...todo];
+        if(inputValue!==''){todoCopy.unshift(inputValue)}
+        setTodo(todoCopy);
+    }
     return(
-        <div>
-
+        <div className={styles.TodoInput__wrap}>
+            <input onChange={(e)=>setInputValue(e.target.value)} type="text" placeholder="오늘 할일 추가!" maxLength={20}></input>
+            <button onClick={addTodo}></button>
         </div>
     )
 }
-
 export default TodoInput;
